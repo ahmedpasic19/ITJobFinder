f2bb2cc10b795bdc293309502538d6a0212e8770
ahmedpasic19 <ahmedpasic15@gmail.com>
2023-09-23 17:39:41 +0200

    Job details footer

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Linking } from 'react-native'
+
+import { icons } from '../../../constants'
 
 import styles from './footer.style'
 
-const Footer = () => {
+const Footer = ({ url }) => {
   return (
-    <View>
-      <Text>Footer</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.likeBtn}
+        onPress={() => Linking.openURL(url)}
+      >
+        <Image
+          source={icons.heart}
+          resizeMode='contain'
+          style={styles.likeBtnImage}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.applyBtn}
+        onPress={() => Linking.openURL(url)}
+      >
+        <Text style={styles.applyBtnText}>Apply for job</Text>
+      </TouchableOpacity>
     </View>
   )
 }
