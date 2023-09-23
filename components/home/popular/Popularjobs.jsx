7659f90a893c3c8f060e9b99dcecfc4069cc640c
ahmedpasic19 <ahmedpasic15@gmail.com>
2023-09-23 16:17:48 +0200

    PopularJobs list component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState, useEffectf } from 'react'
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native'
+import { useRouter } from 'expo-router'
+import useFetch from '../../../hooks/useFetch'
+
+import { COLORS, SIZES } from '../../../constants'
+
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 
 import styles from './popularjobs.style'
 
 const Popularjobs = () => {
+  const router = useRouter()
+
+  const { data, isError, isLoading } = useFetch('search', {
+    query: 'React developer',
+    num_pages: 1,
+    page: 1,
+  })
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity style={styles.headerTitle}>
+          <Text>Show All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading && !isError ? (
+          <ActivityIndicator />
+        ) : !isLoading && isError ? (
+          <Text>Something is wrong</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item}
+            contentContainerStyle={{
+              columnGap: SIZES.medium,
+            }}
+            horizontal
+          />
+        )}
+      </View>
     </View>
   )
 }
